abef29e48984e9951ed5aa8a2d8d3457d2ca3d98
Pablo Rosana <pablogrosana@gmail.com>
2021-10-02 18:45:28 -0300

    First commit.

---
+++ test/helloMocha.js
@@ -0,0 +1,54 @@
+// Addition
+// Substraction
+// Multiplication
+// Division
+
+var assert = require('assert');
+
+describe('Mathematical Operations - Test Suite', function(){
+
+    it('Addition of two numbers', function(){
+
+        var a = 10;
+        var b = 10;
+
+        var c = a + b;
+
+        assert.equal(c, 20);
+
+    });
+
+    it('Substraction of two numbers', function(){
+
+        var a = 10;
+        var b = 10;
+
+        var c = a - b;
+
+        assert.equal(c, 0);
+
+    });
+
+    it('Multiplication of two numbers', function(){
+
+        var a = 10;
+        var b = 10;
+
+        var c = a * b;
+
+        assert.equal(c, 100);
+
+    });
+
+    it('Division of two numbers', function(){
+
+        var a = 10;
+        var b = 10;
+
+        var c = a / b;
+
+        assert.equal(c, 1);
+
+    });
+
+});
